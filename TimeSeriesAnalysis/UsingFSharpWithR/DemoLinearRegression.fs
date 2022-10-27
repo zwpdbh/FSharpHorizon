4b43d1161ff53ff8b9778e7568365c8d558d1fa3
zwpdbh <hyperion_z@outlook.com>
2022-10-27 22:49:50 +0800

    < update

---
+++ TimeSeriesAnalysis/UsingFSharpWithR/DemoLinearRegression.fs
@@ -0,0 +1,37 @@
+﻿module DemoLinearRegression
+
+open RDotNet
+open RProvider
+open RProvider.Operators
+
+open RProvider.graphics
+open RProvider.stats
+
+let linearRegressionDemo () = 
+    
+    // Random number generator
+    let rng = System.Random()
+    let rand () = rng.NextDouble()
+
+    // Generate fake X1 and X2 
+    let X1s = [ for i in 0 .. 9 -> 10. * rand () ]
+    let X2s = [ for i in 0 .. 9 -> 5. * rand () ]
+
+    // Build Ys, following the "true" model
+    let Ys = [ for i in 0 .. 9 -> 5. + 3. * X1s.[i] - 2. * X2s.[i] + rand () ]
+   
+    let dataset = [ 
+        "Y" => Ys
+        "X1" => X1s
+        "X2" => X2s ] |> R.data_frame
+
+    let result = R.lm(formula = "Y~X1+X2", data = dataset)
+
+    let coefficients = result.AsList().["coefficients"].AsNumeric()
+    let residuals = result.AsList().["residuals"].AsNumeric()
+
+    let summary = R.summary(result)
+    summary.AsList().["r.squared"].AsNumeric() |> ignore
+
+    R.plot result
+
--- TimeSeriesAnalysis/UsingFSharpWithR/Program.fs
+++ TimeSeriesAnalysis/UsingFSharpWithR/Program.fs
@@ -1,30 +1,5 @@
-﻿/// See requirement: https://github.com/fslaborg/RProvider#requirements
-/// Note: on Windows, there is currently a bug in R preventing us from supporting R versions greater than 4.0.2.
-open RProvider
-//#r "nuget:RProvider"
+﻿open DemoChart
+open DemoLinearRegression
 
-open RProvider.graphics
-open RProvider.grDevices // Required package to save charts
-open RProvider.datasets
-open RProvider.Helpers
-
-open System
-
-let chartDemo () = 
-    // See: http://bluemountaincapital.github.io/FSharpRProvider/Charts-QuickStart.html
-    // Create path to an image testimage.png on the Desktop
-    let desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)  
-    let path = desktop + @"\testimage.png"
-
-    let widgets = [ 3; 8; 12; 15; 19; 18; 18; 20; ]
-
-    // Open the device and create the file as a png.
-    // R.bmp, R.jpeg, R.pdf, ... will generate other formats.
-    R.png(filename=path, height=200, width=300, bg="white") |> ignore
-    // Create the chart into the file
-    R.barplot(widgets) |> ignore
-    // Close the device once the chart is complete
-    R.dev_off ()
-
-
-chartDemo () |> ignore
+// chartDemo () |> ignore
+linearRegressionDemo () |> ignore
