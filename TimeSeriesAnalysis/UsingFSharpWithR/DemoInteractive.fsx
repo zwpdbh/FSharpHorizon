5c01e27c7e4ab4895e82041574bb5ad5abe2995c
zwpdbh <hyperion_z@outlook.com>
2022-11-05 18:19:47 +0800

    <<<

---
+++ TimeSeriesAnalysis/UsingFSharpWithR/DemoInteractive.fsx
@@ -0,0 +1,27 @@
+
+#r "nuget:RProvider"
+
+
+open RDotNet
+open RProvider
+open RProvider.Operators
+
+open RProvider.graphics
+open RProvider.stats
+
+let rng = System.Random()
+let rand () = rng.NextDouble()
+
+let X1s = [ for i in 0 .. 9 -> 10. * rand () ]
+
+let X2s = [ for i in 0 .. 9 -> 5. * rand () ]
+let Ys = [ for i in 0 .. 9 -> 5. + 3. * X1s.[i] - 2. * X2s.[i] + rand () ]
+
+let dataset = [ 
+        "Y" => Ys
+        "X1" => X1s
+        "X2" => X2s ] |> R.data_frame
+
+let result = R.lm(formula = "Y~X1+X2", data = dataset)
+
+R.plot result
