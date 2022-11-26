d834db5646324abc44ddf1fa2a8945a1f41f0187
zwpdbh <hyperion_z@outlook.com>
2022-11-26 17:37:13 +0800

    >

--- FSharpInAction/Interviews/Optiver/Optiver.fs
+++ FSharpInAction/Interviews/Optiver/Optiver.fs
@@ -1,16 +1,57 @@
 ﻿module Optiver
 
 open Expecto
+open System
+open System.IO
 
-let speedRecordsPath = "@.\Optiver\speed.txt"
+let workingDirectory = Environment.CurrentDirectory;
+let projectFolder = Directory.GetParent(workingDirectory).Parent.Parent.FullName
 
-//let loadSpeedRecord filePath = 
-//    System.IO.
+let speedRecordsFile = "Optiver\speed.txt"
+
+let loadSpeedRecord filePath = 
+    File.ReadAllLines filePath 
+    |> List.ofArray 
+    |> List.filter (fun x -> x.Trim() <> "")
+    |> List.map (fun x -> int x)
+
+// Find the maximum average speed over 1 km
+// Each element in the list is a distance run in 5 mins
+let maxAverageSpeed (lst: int list) = 
+    let mutable d = 0
+    let mutable acc = [] : int list 
+    let segment = 
+        seq {        
+            for v in lst do 
+                d <- d + v 
+                acc <- acc @ [v]
+
+                if d >= 1000 then 
+                    let mutable currD = 0
+                    acc <-
+                        acc 
+                        |> List.rev 
+                        |> List.takeWhile (fun x -> 
+                            currD <- x + currD
+                            currD <= 1000
+                        )
+                    yield acc 
+        }
+    segment 
+    |> Seq.sortBy (fun each -> each.Length)
+    |> Seq.head
 
 let test01 = 
     testCase "01: max average speed" 
     <| fun _ -> 
-        Expect.isTrue true ""
+        let recordFilePath = Path.Combine(projectFolder, speedRecordsFile)
+
+        let count = loadSpeedRecord recordFilePath |> List.length
+        Expect.equal count 491 "The length of speed records"
+
+        let dis = maxAverageSpeed (loadSpeedRecord recordFilePath) |> List.sum 
+        Expect.isTrue (dis >= 1000) ""
+        
 
 [<Tests>]
 let tests = 
--- Playground/Program.fs
+++ Playground/Program.fs
@@ -7,15 +7,49 @@ open FunAndProfit
 //timeRegulareExpression n |> printfn "regulare expression takes: %A" 
 //timeStringContain n |> printfn "string contains takes: %A" 
 
-let mySum lst1 lst2 = 
-    let rec aux acc lst1 lst2 = 
-        match lst1, lst2 with 
-        | x::rest1, y::rest2 -> 
-            aux (acc + x * y) rest1 rest2 
-        | [], list2 -> acc + List.sum list2 
-        | list1, [] -> acc + List.sum list1 
 
-    aux 0 lst1 lst2 
+open System.IO
 
-let testResult = Dynamic.Call mySum [[1;2;3]; [1;2;3;100]]
-printfn "%A" testResult
+
+let speedFile = @"D:\code\fsharp-programming\FSharpHorizen\FSharpInAction\Interviews\Optiver\speed.txt"
+
+let loadSpeedRecord filePath = 
+    File.ReadAllLines filePath 
+    |> List.ofArray 
+    |> List.filter (fun x -> x.Trim() <> "")
+    |> List.map (fun x -> int x)
+
+// Find the maximum average speed over 1 km
+// Each element in the list is a distance run in 5 mins
+let maxAverageSpeed (lst: int list) = 
+    let mutable d = 0
+    let mutable acc = [] : int list 
+    let segment = 
+        seq {        
+            for v in lst do 
+                d <- d + v 
+                acc <- acc @ [v]
+
+                if d > 1000 then 
+                    let mutable currD = 0
+                     
+                    acc <-
+                        acc 
+                        |> List.rev 
+                        |> List.takeWhile (fun x -> 
+                            currD <- x + currD
+                            currD <= 1000
+                        )
+                        |> List.rev 
+                    yield acc
+                    
+        }
+    segment 
+    |> Seq.sortBy (fun each -> each.Length)
+
+
+let result = 
+    maxAverageSpeed (loadSpeedRecord speedFile)
+
+for each in result do 
+    printfn $"{each} : {List.sum each}"
