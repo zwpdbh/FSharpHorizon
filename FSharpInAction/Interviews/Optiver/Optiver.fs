3ee16ba37f672248c79e3164e2a9d0eec460e0d5
zwpdbh <hyperion_z@outlook.com>
2022-11-26 12:59:29 +0800

    > find optiver solution

---
+++ FSharpInAction/Interviews/Optiver/Optiver.fs
@@ -0,0 +1,17 @@
+﻿module Optiver
+
+open Expecto
+
+let speedRecordsPath = "@.\Optiver\speed.txt"
+
+//let loadSpeedRecord filePath = 
+//    System.IO.
+
+let test01 = 
+    testCase "01: max average speed" 
+    <| fun _ -> 
+        Expect.isTrue true ""
+
+[<Tests>]
+let tests = 
+    testList "From Optiver " [test01]
