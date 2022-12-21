34baec8737aa2b1ee4e0488d4fe4a48bcb3200df
zwpdbh <hyperion_z@outlook.com>
2022-12-21 10:21:34 +0800

    <update DDD

---
+++ Playground/DemoDDD.fs
@@ -0,0 +1,25 @@
+﻿namespace DDD 
+
+
+module DemoDDD = 
+    type ValidateAddress = private ValidateAddress of string 
+
+    module ValidateAddress = 
+        let create (address: string) = 
+            if address.Length <= 4 then 
+                Error "Address must be at least 5 characters"
+            else if address.Length > 30 then 
+                Error "Address must be no longer than 30 characters"
+            else 
+                Ok (ValidateAddress address)
+
+        let value (ValidateAddress address) = address 
+
+    let demo() = 
+        let validAddressResult = ValidateAddress.create "some"
+        match validAddressResult with 
+        | Error msg -> 
+            failwith msg 
+        | Ok validAddress -> 
+            let innerValue = ValidateAddress.value validAddress
+            printfn "inner value is %A" innerValue
