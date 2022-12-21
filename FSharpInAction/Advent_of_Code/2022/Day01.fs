7d4b06f85c32bb59bf378bd963185c27c4b05c69
zwpdbh <hyperion_z@outlook.com>
2022-12-21 22:46:14 +0800

    <day 01 finished

--- FSharpInAction/Advent_of_Code/2022/Day01.fs
+++ FSharpInAction/Advent_of_Code/2022/Day01.fs
@@ -23,19 +23,36 @@ let findMaxCalorie (numbers: string array) =
     |> List.sortDescending
     |> List.head
 
+let findTop3TotalCalorie (numbers: string array) = 
+    numbers
+    |> List.ofArray
+    |> groupCalorie
+    |> List.map (fun each -> each |> List.map (fun x -> int x) |> List.sum)
+    |> List.sortDescending
+    |> List.indexed
+    |> List.takeWhile (fun (i, _) -> i < 3)
+    |> List.sumBy (fun (_, v) -> v)
+
+
+let workingDirectory = Environment.CurrentDirectory
+// We need to go to its "../../" because it is running from output folder
+let projectFolder = Directory.GetParent(workingDirectory).Parent.Parent.FullName
+
+let numbers =
+    File.ReadAllLines(Path.Combine(projectFolder, @"2022\input\day01.txt"))
+
 
 // For https://adventofcode.com/2022/day/1
 let test01 =
     testCase "Part one"
     <| fun _ -> 
-        let workingDirectory = Environment.CurrentDirectory
-        // We need to go to its "../../" because it is running from output folder
-        let projectFolder = Directory.GetParent(workingDirectory).Parent.Parent.FullName
-
-        let numbers =
-            File.ReadAllLines(Path.Combine(projectFolder, @"2022\input\day01.txt"))
         Expect.equal (findMaxCalorie numbers) 71506 "The most total Calories carried by Elf"
 
 
+let test02 =
+    testCase "Part two"
+    <| fun _ -> 
+        Expect.equal (findTop3TotalCalorie numbers) 209603 "The most total Calories carried by Elf"
+
 [<Tests>]
-let tests = testList "Day01" [ test01 ]
+let tests = testList "Day01" [ test01; test02 ]
