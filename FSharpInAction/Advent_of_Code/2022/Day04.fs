2eccae78a677d3cfb4341601933ab9e57aec38a5
zwpdbh <hyperion_z@outlook.com>
2022-12-23 20:33:52 +0800

    > day04

---
+++ FSharpInAction/Advent_of_Code/2022/Day04.fs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022
+open Expecto
+
+module Day04 = 
+    module Part01 = 
+        let test01 = 
+            testCase "Part01 baseline"
+            <| fun _-> 
+                Expect.isTrue true ""
+
+
+    [<Tests>]
+    let tests = testList "Day04" [Part01.test01]
+
