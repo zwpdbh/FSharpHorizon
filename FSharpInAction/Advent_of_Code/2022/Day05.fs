bfa573f5f23a3a9637c72bacad82a1d5a2ddb384
zwpdbh <hyperion_z@outlook.com>
2022-12-25 23:18:10 +0800

    > day05

--- FSharpInAction/Advent_of_Code/2022/Day05.fs
+++ FSharpInAction/Advent_of_Code/2022/Day05.fs
@@ -3,8 +3,12 @@ open Expecto
 
 module Day05 = 
 
-
+    module Part01 = 
+        let test01 = 
+            testCase "baseline"
+            <| fun _ -> 
+                Expect.isTrue true "baseline"
 
     [<Tests>]
-    let tests = testList "Day05" []
+    let tests = testList "Day05" [Part01.test01]
 
