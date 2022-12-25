26b6b7a1959481d3e7e25122ee869a21fe59a4bc
zwpdbh <hyperion_z@outlook.com>
2022-12-25 15:40:01 +0800

    < Demo01 c vs f

---
+++ WhyFSharp/CSharpDemos/Demo01.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemos
+{
+    public static class Demo01
+    {
+        // For completeness, this is two methods to ensure that the null check 
+        // is done eagerly while the loop is done lazily. If that's not an issue, 
+        // you can forego the check and just use the main function.
+        public static IEnumerable<T> NonConsecutive<T>(this IEnumerable<T> input)
+        {
+            if (input == null) throw new ArgumentNullException("input");
+            return NonConsecutiveImpl(input);
+        }
+
+        static IEnumerable<T> NonConsecutiveImpl<T>(this IEnumerable<T> input)
+        {
+            bool isFirst = true;
+            T last = default(T);
+            foreach (var item in input)
+            {
+                if (isFirst || !object.Equals(item, last))
+                {
+                    yield return item;
+                    last = item;
+                    isFirst = false;
+                }
+            }
+        }
+
+        public static void Demo()
+        {
+            var input01 = new List<int>() { 1, 1, 2, 3, 3, 4, 4, 4 };
+            foreach (int i in NonConsecutive<int>(input01))
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            var input02 = new List<string>() { "a", "a", "b", "b", "c", "c", "c", "a" };
+            foreach (string i in NonConsecutive<string>(input02))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ WhyFSharp/FSharpDemo/Demos.fs
@@ -0,0 +1,36 @@
+﻿namespace WhyFSharp
+
+module Demo01 =
+    // Eliminate duplicates
+    // Shows pattern matching is powerful
+    // Shows it is generic by default
+    // SHows it doesn't accept null
+    let rec compress =
+        function
+        | a :: (b :: _ as tail) ->
+            // Instead of keep the first and ignore following repeated as I first though
+            // It continue to see next until meet different element to keep the last one !
+            if a = b then
+                compress tail
+            else
+                a :: (compress tail)
+        | x -> x
+
+    let demo01 () =
+        compress [ "a"
+                   "a"
+                   "a"
+                   "a"
+                   "b"
+                   "c"
+                   "c"
+                   "a"
+                   "a"
+                   "d"
+                   "e"
+                   "e"
+                   "e"
+                   "e" ]
+
+    let demo02 () = 
+        compress [1; 1; 2; 2; 3; 3; 3; 3; 4]
