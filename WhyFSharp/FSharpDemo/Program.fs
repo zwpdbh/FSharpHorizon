31375f0fce8aa793146ed2b6ba350a939dd92fa4
zwpdbh <hyperion_z@outlook.com>
2022-12-27 17:20:31 +0800

    < read for demo

--- WhyFSharp/FSharpDemo/Program.fs
+++ WhyFSharp/FSharpDemo/Program.fs
@@ -1,6 +1,20 @@
-﻿
+﻿/// What is F#: https://dotnet.microsoft.com/en-us/languages/fsharp
+/// F# is An open-source language that makes it easy to write succinct, robust, and performant code -- from Microsft
+
 /// Show: https://fsharpforfunandprofit.com/why-use-fsharp/
+/// Why it could achieve: Conciseness, Convenience, Correctness, Concurrency and Completeness
+/// Because F# is a functional programming First language!
+
+/// Functional programming matters: 
+/// Immutable by default + Expression => make things much easier to understand and to reason about.
+
+/// FSharp achieve balance between Functional Programming and OOP (or Mutable)
+/// Show Examples between F# and C#
+
 
 //WhyFSharp.Demo07.demo() 
 
 WhyFSharp.Demo08.demo()
+
+
+/// Talk about my experience about choosing F# 
