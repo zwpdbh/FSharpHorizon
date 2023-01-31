0ed1c9296f989e31d2c24dd446cec91157cae371
zwpdbh <hyperion_z@outlook.com>
2023-01-31 23:07:25 +0800

    < review

---
+++ FSharpInAction/OtherTopics/ParserCombinators/Combinators.fs
@@ -0,0 +1,150 @@
+﻿namespace OtherTopics
+open Expecto
+
+module Combinators = 
+    // For https://www.youtube.com/watch?v=ARJB8eDyxrg
+    // The commands we want to parse are
+    // go west
+    // take key 
+    // take key from bag
+
+    type Parser<'a> = 
+        | Parser of (char list -> Result<'a * char list, string>)
+    
+    let runParser parser inputChars = 
+        match parser with 
+        | Parser parserFun -> 
+            parserFun inputChars
+
+    let expectChar expChar = 
+        let innerParser inputChars = 
+            match inputChars with 
+            | c :: remainingChars ->
+                if c = expChar then 
+                    Result.Ok (c, remainingChars)
+                else 
+                    Result.Error ($"Expected {expChar}, got {c}")
+            | _ -> 
+                Result.Error ($"Expected {expChar}, but reach end of inputChar")
+
+        Parser innerParser
+
+    let strToCharList str =
+        str |> List.ofSeq
+
+    // The idea of combinator: combine two parser function to achieve more complex behaviour. 
+
+    // Or Parser
+    let orParse pFun1 pFun2 = 
+        let innerParser inputChars = 
+            match runParser pFun1 inputChars with 
+            | Result.Ok result -> Result.Ok result 
+            | Result.Error _ -> 
+                runParser pFun2 inputChars
+        Parser innerParser
+
+    // Define operation 
+    // Operator definitions are left associative 
+    let ( <|> ) = orParse
+
+    let choice parserList = 
+        List.reduce orParse parserList
+
+    let anyCharOf validChars = 
+        validChars
+        |> List.map expectChar
+        |> choice
+
+    let andParse pFun1 pFun2 = 
+        let innerParser inputChars = 
+            match runParser pFun1 inputChars with 
+            | Result.Ok (c1, remaining1) -> 
+                match runParser pFun2 remaining1 with 
+                | Result.Ok (c2, remaining2) -> 
+                    Result.Ok ((c1, c2), remaining2)
+                | Result.Error err -> Result.Error err 
+            | Result.Error err -> Result.Error err 
+
+        Parser innerParser
+
+    let ( .>>. ) = andParse
+
+    let mapParser mapFun parser = 
+        let innerParser inputChars = 
+            match runParser parser inputChars with 
+            | Result.Error msg -> Result.Error msg 
+            | Result.Ok (result, remainingChars) ->
+                Result.Ok (mapFun result, remainingChars)
+
+        Parser innerParser
+
+    let applyParser funcAsParser paramAsParser = 
+        (funcAsParser .>>. paramAsParser)
+        |> mapParser (fun (f, x) -> f x)
+
+    let ( <*> ) = applyParser
+
+    let returnAsParser result = 
+        let innerParser inputChars = 
+            Result.Ok (result, inputChars)
+
+        Parser innerParser
+
+    let liftToParser2 funcToLift paramAsParser1 paramAsParser2 = 
+        returnAsParser funcToLift <*> paramAsParser1 <*> paramAsParser2
+
+    let rec sequenceParsers parserList = 
+        let cons head rest = head :: rest 
+        let consAsParser = liftToParser2 cons 
+
+        match parserList with 
+        | parser :: remainerParsers -> 
+            consAsParser parser  (sequenceParsers remainerParsers)
+        | [] -> 
+            returnAsParser []
+
+
+    let expectString expectString = 
+        expectString
+        |> strToCharList
+        |> List.map expectChar
+        |> sequenceParsers
+        |> mapParser (fun chars -> System.String(List.toArray chars))
+
+module CombinatorsDemos = 
+    open Combinators
+
+    // OR 
+    let demo01 () = 
+        strToCharList "rake"
+        |> (expectChar 'r' <|> expectChar 't' <|> expectChar 'c')  
+
+    let demo02 () = 
+        strToCharList "rake"
+        |> choice [expectChar 'r'; expectChar 'a'; expectChar 't']  
+
+    let demo03 () = 
+        strToCharList "wake"
+        |> anyCharOf ("rkaw" |> List.ofSeq)
+
+
+    // AND 
+    let demo04 () = 
+        strToCharList "rake"
+        |> andParse (expectChar 'r') (expectChar 'a')
+
+    let demo05 () = 
+        strToCharList "rake"
+        |> (expectChar 'r' .>>. expectChar 'a')
+
+    let demo05 () = 
+        strToCharList "rake"
+        |> runParser (expectChar 'r')
+
+    let demo06 () = 
+        strToCharList "rake"
+        |> runParser (expectString "rake")
+
+    let demo07 () = 
+        strToCharList "rake"
+        |> runParser (expectString "rake" <|> expectString "lake")
