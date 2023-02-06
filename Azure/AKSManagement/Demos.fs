6e29727474516cd4a4566e4f8d030e0a4e763c46
zwpdbh <hyperion_z@outlook.com>
2023-02-06 16:46:45 +0800

    > update project structures

--- Azure/AKSManagement/Demos.fs
+++ Azure/AKSManagement/Demos.fs
@@ -1,4 +1,25 @@
 ﻿namespace AKSManagement
+
+module Snapshot = 
+    open AzureAPI
+    let listAllSnapshots (subscriptionId: string ) (accessToken: string) = 
+
+        // GET https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Compute/snapshots?api-version=2021-12-01
+        let endpoint = $"https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Compute/snapshots"
+        let queryParameters: Map<string, string> = 
+            Map.empty
+                .Add ("api-version", "2021-12-01")
+
+        let headerParameters: Map<string, string> = 
+            Map.empty
+                .Add("Authorization", $"Bearer {accessToken}")
+
+        async {
+            let! responseStr = HttpClient.getRequestAsync endpoint (Some headerParameters) (Some queryParameters)
+            return responseStr
+        }
+
+
 module Demos = 
     let demoListSnapshot () : string = 
         async {
---
+++ Azure/XscnScenario/XscnScenarioDemos.fs
@@ -0,0 +1,10 @@
+﻿namespace XscnXscnario
+
+/// References:
+/// https://xscenariodeployments.cloudapp.net/swagger/ui/index#!/VirtualMachineDeployments/VirtualMachineDeployments_PutDeployment
+/// ArmTemplates stored at: https://msazure.visualstudio.com/One/_git/Storage-XPerfInfra?path=/etc/ArmTemplates
+
+module XscnScenarioDemos = 
+    let listVirtualMachineDeployments () =
+        printfn "done"
+
