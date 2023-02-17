01da12395622029b9fadc745516bdbf4687a4531
zwpdbh <hyperion_z@outlook.com>
2023-02-17 13:20:24 +0800

    <fix sql file error

--- FSharpInAction/Others/WorkingWithData/PostgreSQL/db-init.sql
+++ FSharpInAction/Others/WorkingWithData/PostgreSQL/db-init.sql
@@ -2,14 +2,14 @@
 -- Database: fsharp
 
 -- DROP DATABASE IF EXISTS fsharp;
-drop table if exists Course CASCADE; 
+drop table if exists Course; 
 
 create table Course (
    Id serial primary key,
    Name varchar (50) NOT NULL
 );
 
-drop table if exists Student CASCADE;
+drop table if exists Student ;
 create table Student (
 	Id serial primary key,
 	Name varchar (50) NOT NULL,
@@ -19,8 +19,8 @@ create table Student (
 drop table if exists CourseSelection;
 create table CourseSelection (
 	Id serial primary key,
-	StudentId int references Student(id),
-	CourseId int references Course(id)
+	StudentId int references Student(id) on delete cascade,
+	CourseId int references Course(id) on delete cascade,
 );
 
 drop table if exists LastStudent;
