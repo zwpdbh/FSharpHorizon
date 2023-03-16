c2b69f53fef1f16a63ce74c76c2c49439b2464ec
zwpdbh <hyperion_z@outlook.com>
2023-03-16 17:07:47 +0800

    <

--- FSharpInAction/Others/Program.fs
+++ FSharpInAction/Others/Program.fs
@@ -3,34 +3,10 @@ namespace Others
 
 module OthersMain =
     open Expecto
-
+    open System.Text.RegularExpressions
     [<EntryPoint>]
     let main argv =
-        //Tests.runTestsInAssembly defaultConfig argv
 
-        //Others.WorkingWithData.PostgreSql.demoInsertOneRow () 
-        //Others.WorkingWithData.PostgreSql.insertStudents ()
-        //Others.WorkingWithData.PostgreSql.NpgsqlFSharp.initDB ()
-
-        //Others.WorkingWithData.PostgreSql.SqlProvider.demos ()
-
-        
-        // Common.readInput @"FromInterview\words.txt" |> printfn "%A"
-        //Common.filePathInProject @"ComputationExpression\PNG_01.png" |> printfn "%A"
-
-        //Snippets.ProcessText.Frequence.demoMostFrequence () |> printfn "%A"
-        //Snippets.SlideWindow.SubStringProblem.demo () |> printfn "%A"
-        //Snippets.SlideWindow.SubStringProblemActivePattern.demo () |> printfn "%A"
-        //FromInterview.Polymer.Problem01.demo () |> printfn "%A"
         FromInterview.Tmp.Problem01.demo () |> printfn "%A"
 
-        //FromInterview.Ahrefs.Problem01.demo () |> printfn "%A"
-        //Others.Snippets.SlideWindow.SubString.demo () 
-
-        //Others.Snippets.DecodeJson.DecodeMapResponse.demo() |> printfn "%A"
-        //Others.Snippets.DecodeJson.DecodeWorkflowsResponseV3.demo () |> printfn "%A"
-
-
-        //Others.Snippets.WsClient.WebSocketsClient.demo () 
-        //Others.Snippets.WsClient.WebSocketClient.startStreaming "ethusd" 
         0
