5a16253faf1bc35bf40f4b9f55107f8195879776
zwpdbh <hyperion_z@outlook.com>
2023-04-10 09:55:11 +0800

    > for linux environment

---
+++ Azure/AksManagement/Program.fs
@@ -0,0 +1,11 @@
+namespace AksManagement
+module Main =
+    [<EntryPoint>]
+    let main argv =
+        0
+        // For more information see https://aka.ms/fsharp-console-apps
+
+
+        //AksManagement.Snapshot.demoListSnapshot ()
+        //AksManagement.XscnWorkflowConsole.demoListWorkflowInstances()
+
